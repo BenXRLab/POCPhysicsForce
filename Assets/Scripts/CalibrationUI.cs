ba84379f148b9b30fb267ff9b8ad1afd71c18893
Karolina <w23722137@student.wsc.ac.uk>
2025-11-27 10:28:08 +0000

    Status Display Update
    CalibrationUI.cs script can now show different outputs based on the trial status. If the user has correctly calibrated the trolley and weights, when they press the start trial button they will be able to see text "Trial Running..."

--- Assets/Scripts/CalibrationUI.cs
+++ Assets/Scripts/CalibrationUI.cs
@@ -2,31 +2,52 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using System;
 
 public class CalibrationUI : MonoBehaviour
 {
+    // Text outputs
     private const string not_calibrated = "Please place trolley and weight on the track.";
     private const string calibrated = "Calibration Complete.";
+    private const string Trial_Text = "Trial Running...";
     // calibration is a test variable, remove when CalibrationCheck.cs is made
     private bool calibration = true;
+    private bool Trial_Active = false;
     public TextMeshProUGUI TextField;
     private string newText;
 
 
-    // Triggers a delay if button is pressed
 
 
-    // changes text on the panel
+
+    //Used by Start Trial button to change trial status
+    public void Trial_Activate()
+    {
+        Trial_Active = true;
+    }
+    //Currently unused function to set trial status to inactive
+    public void Trial_Deactivate()
+    {
+        Trial_Active = false;
+    }
+
+
+    // changes text on the panel using 3 options
     // updates the result every frame
-    // Starts an update delay timer if the start trial button is pressed
-
-
     void Update()
     { 
             if (calibration == true)
             {
+                if (Trial_Active)
+                {
+                newText = Trial_Text;
+                TextField.text = newText;
+                }
+                else
+                {
                 newText = calibrated;
                 TextField.text = newText;
+                }
 
             }
             else
@@ -36,4 +57,5 @@ public class CalibrationUI : MonoBehaviour
             }
     }
 }
+
     
--- Assets/Scripts/Trial_Text.cs
+++ Assets/Scripts/Trial_Text.cs
@@ -1,24 +1,10 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using System;
 public class Trial_Text : MonoBehaviour
 {
-    public TextMeshProUGUI TextField;
+    public CalibrationUI Trial_Active;
 
-
-    //Sets Text to Trial not Running
-    private void OldText()
-    {
-        string oldText = "Trial not Running";
-        TextField.text = oldText;
-    }
-    // Sets text to Trial Running
-    public void NewText()
-    {
-        string oldText = "Trial Running...";
-        TextField.text = oldText;
-    }
 
 }
